3b402384cd21bc57c85821886436cfa4fa7983f1
BobGame <liulingbo@foxmail.com>
2023-05-09 10:42:19 +0800

    test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
+
 const config: CapacitorConfig = {
-  appId: "com.llbnew.newtestbybobll",
-  appName: "Photo Gallery Cap Ng",
-  bundledWebRuntime: false,
-  npmClient: "npm",
-  webDir: "www",
+  appId: 'com.llbnew.newtestbybobll',
+  appName: 'new-test-by-bob-2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
